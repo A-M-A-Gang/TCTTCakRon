c3ce67299538ee926020f158e864fade36ef5042
Angga Maulana Athaariq <am.athaariq@gmail.com>
2019-10-03 10:45:17 +0700

    Fitur Admin

---
+++ app/src/main/java/id/ac/polinema/tcttcakron/FiturAdmin.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.tcttcakron;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FiturAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fitur_admin);
+    }
+}
---
+++ app/src/main/java/id/ac/polinema/tcttcakron/UpdateAdmin.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.tcttcakron;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_admin);
+    }
+}
