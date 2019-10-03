0f0df0da4c3e6382e95c6ea8ec7fe4e53e09e72f
Angga Maulana Athaariq <am.athaariq@gmail.com>
2019-10-03 12:09:13 +0700

    Fitur Admin ( Update )

---
+++ app/src/main/java/id/ac/polinema/tcttcakron/TransactionAdmin.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.tcttcakron;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransactionAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_admin);
+    }
+}
