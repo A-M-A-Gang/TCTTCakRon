8e699ce07de62780145851751c2f5e9af65015d9
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-10-07 20:51:15 +0700

    Login Button

--- app/src/main/java/id/ac/polinema/tcttcakron/LoginAdmin.java
+++ app/src/main/java/id/ac/polinema/tcttcakron/LoginAdmin.java
@@ -3,6 +3,7 @@ package id.ac.polinema.tcttcakron;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginAdmin extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class LoginAdmin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_admin);
+    }
+
+    public void handlerOnClickLogin(View view) {
+        setContentView(R.layout.activity_fitur_admin);
     }
 }
