1a64b287c2c7a363a6d5f94105899c4596eab270
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-10-08 20:25:08 +0700

    Update Button on Fitur Admin

--- app/src/main/java/id/ac/polinema/tcttcakron/FiturAdmin.java
+++ app/src/main/java/id/ac/polinema/tcttcakron/FiturAdmin.java
@@ -2,7 +2,9 @@ package id.ac.polinema.tcttcakron;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FiturAdmin extends AppCompatActivity {
 
@@ -10,5 +12,31 @@ public class FiturAdmin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fitur_admin);
+    }
+
+    public void handlerOnClickUpdate(View view) {
+        Intent intent = new Intent(this, UpdateAdmin.class);
+        startActivity(intent);
+    }
+
+    public void handlerOnClickTransaction(View view) {
+        Intent intent = new Intent(this, TransactionAdmin.class);
+        startActivity(intent);
+    }
+
+
+    public void handlerOnClickAdd(View view) {
+        Intent intent = new Intent(this, AddAdmin.class);
+        startActivity(intent);
+    }
+
+    public void handlerOnClickDelete(View view) {
+        Intent intent = new Intent(this, DeleteAdmin.class);
+        startActivity(intent);
+    }
+
+    public void handlerOnClickLogout(View view) {
+        Intent intent = new Intent(this, LoginAdmin.class);
+        startActivity(intent);
     }
 }
