054366bc20136a504980bcd5cdc53c3401905d19
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-10-08 21:51:13 +0700

    Add onclick on Trasanction

--- app/src/main/java/id/ac/polinema/tcttcakron/TransactionAdmin.java
+++ app/src/main/java/id/ac/polinema/tcttcakron/TransactionAdmin.java
@@ -2,7 +2,9 @@ package id.ac.polinema.tcttcakron;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TransactionAdmin extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class TransactionAdmin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_admin);
+    }
+
+    public void handlerOnClickOffline(View view) {
+        Intent intent = new Intent(this, TransactionOffline.class);
+        startActivity(intent);
+    }
+
+    public void handlerOnClickOnline(View view) {
+        Intent intent = new Intent(this, TransactionOnline.class);
+        startActivity(intent);
     }
 }
