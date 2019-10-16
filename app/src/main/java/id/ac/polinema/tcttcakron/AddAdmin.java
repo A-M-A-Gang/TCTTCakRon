51d1d82bc4740bcf7d232a13f3a234da57d641b0
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-10-16 12:07:09 +0700

    Update Layout & Function

--- app/src/main/java/id/ac/polinema/tcttcakron/AddAdmin.java
+++ app/src/main/java/id/ac/polinema/tcttcakron/AddAdmin.java
@@ -2,7 +2,9 @@ package id.ac.polinema.tcttcakron;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddAdmin extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class AddAdmin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_admin);
+    }
+
+    public void handlerOnClickBatal(View view) {
+        Intent intent = new Intent(this, FiturAdmin.class);
+        startActivity(intent);
     }
 }
