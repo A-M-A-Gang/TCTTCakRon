e80f832995a60d389da91701df495ef4bef07195
Angga Maulana Athaariq <am.athaariq@gmail.com>
2019-10-23 16:04:59 +0700

    update fungsi Add menu

--- app/src/main/java/id/ac/polinema/tcttcakron/Upload.java
+++ app/src/main/java/id/ac/polinema/tcttcakron/Upload.java
@@ -4,31 +4,38 @@ public class Upload {
     private String nameImage, imageUrl;
     private int harga;
 
+
     public Upload(String nameImage, String imageUrl, int harga) {
+        if (nameImage.trim().equals("")) {
+            nameImage = "No Name";
+        }
         this.imageUrl = imageUrl;
         this.nameImage = nameImage;
         this.harga = harga;
+
     }
 
     public String getNameImage() {
         return nameImage;
     }
 
-//    public void setNameImage(String nameImage) {
-//        this.nameImage = nameImage;
-//    }
+    public void setNameImage(String nameImage) {
+        this.nameImage = nameImage;
+    }
+
     public String getImageUrl() {
         return imageUrl;
     }
 
-//    public void setImageUrl(String imageUrl) {
-//        this.imageUrl = imageUrl;
-//    }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
     public int getHarga() {
         return harga;
     }
 
-//    public void setHarga(int harga) {
-//        this.harga = harga;
-
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
 }
