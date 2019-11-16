badad87772c4ab61ad99fcacea204a2ac545340a
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-11-16 15:58:23 +0700

    Update history page

--- TCTTCakRon/app/src/main/java/id/ac/polinema/tcttcakron/HistoryAdmin.java
+++ TCTTCakRon/app/src/main/java/id/ac/polinema/tcttcakron/HistoryAdmin.java
@@ -1,14 +1,70 @@
 package id.ac.polinema.tcttcakron;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class HistoryAdmin extends AppCompatActivity {
+    TextView dateFrom, dateTo;
+
+    Calendar calendar;
+    DatePickerDialog dpd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_admin);
+        dateFrom = findViewById(R.id.display_from_date);
+        dateTo = findViewById(R.id.display_to_date);
+
+        Date c = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        String formattedDate = df.format(c);
+        dateFrom.setText(formattedDate);
+        dateTo.setText(formattedDate);
+
+        dateFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                dpd = new DatePickerDialog(HistoryAdmin.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int mYear, int mMonth, int mDay) {
+                        dateFrom.setText(mDay + "/" + (mMonth + 1) + "/" + mYear);
+                    }
+                }, year, month, day);
+                dpd.show();
+            }
+        });
+
+        dateTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                dpd = new DatePickerDialog(HistoryAdmin.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int mYear, int mMonth, int mDay) {
+                        dateTo.setText(mDay + "/" + (mMonth + 1) + "/" + mYear);
+                    }
+                }, year, month, day);
+                dpd.show();
+            }
+        });
     }
 }
