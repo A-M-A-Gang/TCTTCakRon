a67974639842e874fa5657119fc29d3ae32fea05
Ahmad Musyadad Aminullah <ahmadmusyadadaminullah@gmail.com>
2019-11-26 18:52:50 +0700

    Revert "Fix display on History"
    This reverts commit 1cef5ea17db4a568d80c86f90fc9420b3a806aa9.
    
    # Conflicts:
    #	TCTTCakRon/app/src/main/res/layout/activity_history_admin.xml

--- TCTTCakRon/app/src/main/java/id/ac/polinema/tcttcakron/adapters/TransactionListAdapter.java
+++ TCTTCakRon/app/src/main/java/id/ac/polinema/tcttcakron/adapters/TransactionListAdapter.java
@@ -1,35 +1,40 @@
 package id.ac.polinema.tcttcakron.adapters;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import id.ac.polinema.tcttcakron.R;
+import id.ac.polinema.tcttcakron.models.Upload;
 
 public class TransactionListAdapter extends RecyclerView.Adapter<TransactionListAdapter.ImageViewHolder> {
     private Context mContext;
     private Map.Entry<String, Integer> mUploads;
-    private String[] mTanggalMenu;
-    private int mTotalTransaksi;
 
-    public TransactionListAdapter(Context context, String[] tanggalMenu , int totalTransaksi){
+    public TransactionListAdapter(Context context, Map.Entry<String, Integer> uploads){
         mContext = context;
-        mTanggalMenu = tanggalMenu;
-        mTotalTransaksi = totalTransaksi;
+        mUploads = uploads;
 //        Map.Entry<String, Integer> mUploads2 = (Map.Entry<String, Integer>) new HashMap<String, Integer>();
 //        Set<Map.Entry<String, Integer>> set = mUploads2.entrySet();
 
-//        Map<Integer, String> mapFromSet = new HashMap<Integer, String>();
+        Map<Integer, String> mapFromSet = new HashMap<Integer, String>();
 //        for(Map.Entry<String, Integer> entry : set)
 //        {
 //            mapFromSet.put(entry.getValue(), entry.getKey());
@@ -46,11 +51,8 @@ public class TransactionListAdapter extends RecyclerView.Adapter<TransactionList
     @Override
     public void onBindViewHolder(@NonNull final ImageViewHolder holder, int position) {
 //        final Upload uploadCurrent = mUploads.get(position);
-//        holder.tanggal.setText(mUploads.getKey());
-//        holder.total.setText(mUploads.getValue());
-
-        holder.tanggal.setText(mTanggalMenu[position]);
-        holder.total.setText(String.valueOf(mTotalTransaksi));
+        holder.tanggal.setText(mUploads.getKey());
+        holder.total.setText(mUploads.getValue());
 //        BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
 //            @Override
 //            public void onReceive(Context context, Intent intent) {
@@ -69,15 +71,7 @@ public class TransactionListAdapter extends RecyclerView.Adapter<TransactionList
 //                maxEntry = entry;
 //            }
 //        }
-        int ji = 0;
-        for (int i = 0; i < mTanggalMenu.length; i++){
-            if(mTanggalMenu[i] == null){
-                ji = i;
-                break;
-            }
-            ji = i;
-        }
-        return ji;
+        return mUploads.getValue();
     }
 
     public class ImageViewHolder extends RecyclerView.ViewHolder {
